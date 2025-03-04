4e078551f536d95899df62c496e1ab447e60b022
Yura Okulovsky <yura.okulovsky@outfittery.de>
2025-03-04 11:51:32 +0100

    Trying to retrain a model as the old one stopped working

--- run_names.py
+++ run_names.py
@@ -1,8 +1,29 @@
-from ikeanames.ikeanames import predict_name, load_model, get_encoding, load_names
+from ikeanames.ikeanames import *
+
+class Runner:
+    def __init__(self):
+        self.names = load_names()
+        (self.max_len, self.encoding, self.decoding) = get_encoding(self.names)
+
+    def train(self):
+        (X, y) = make_train_set(self.names, self.encoding, self.decoding, self.max_len)
+        model = make_model(X)
+        train_model(model, X, y, epochs=1000)
+        try:
+            fn = sys.argv[2]
+        except IndexError:
+            fn = None
+        save_model(model, fn)
+
+    def predict(self):
+        model = load_model('models/model.h5')
+        n = 10
+        for i in range(n):
+            name = predict_name(model, self.max_len, self.encoding, self.decoding)
+            if name in self.names:
+                continue
+            print(name)
 
 if __name__ == '__main__':
-    model = load_model()
-    names = load_names()
-    (max_len, encoding, decoding) = get_encoding(load_names()[:1])
-    name = predict_name(model, max_len, encoding, decoding)
-    print(name)
+    r = Runner()
+    r.train()
